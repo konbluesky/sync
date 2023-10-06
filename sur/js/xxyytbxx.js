80b21696a10f8c3171f7d6a54ae1b5a3e74b3bab
adntian <china2008tian@126.com>
2023-10-06 06:24:01 -0500

    Update xxyytbxx.js

--- sur/js/xxyytbxx.js
+++ sur/js/xxyytbxx.js
@@ -1,10 +1,4 @@
-console.log($response)
-console.log($response.body)
 let body = $response.body.replace(/NO</g, "YES<")
-console.log(body)
-	$done({
-    	response: {
-      		status: 200,
-      		body: body
-    	}
-	})
+$done({
+    body: body
+})
