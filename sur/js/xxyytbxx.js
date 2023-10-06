376052c151a30c03a51b8298b901e128f2248e24
adntian <china2008tian@126.com>
2023-10-06 06:11:50 -0500

    Update xxyytbxx.js

--- sur/js/xxyytbxx.js
+++ sur/js/xxyytbxx.js
@@ -1,6 +1,6 @@
 console.log($response)
 console.log($response.body)
-let body = $response.body.replace(/?NO/g, "?YES")
+let body = $response.body.replace(/NO</g, "YES<")
 console.log(body)
 	$done({
     	response: {
